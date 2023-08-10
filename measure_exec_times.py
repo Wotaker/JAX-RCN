24923ce424832e23df9577e26865b52f85994893
Wojciech Ciezobka <wciezobka@student.agh.edu.pl>
2023-08-10 16:47:12 +0200

    exec time measurments

---
+++ measure_exec_times.py
@@ -0,0 +1,147 @@
+import timeit
+from argparse import ArgumentParser
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import jax.numpy as jnp
+import jax
+from pyrcn.base.blocks import InputToNode, NodeToNode
+from sklearn.linear_model import Ridge
+
+from chex import Array
+
+from esn import ESN
+from utils import chunkify
+from gen_mackey_glass import generate_mackey_glass
+
+# Constants
+PRNGKEY = jax.random.PRNGKey(71)
+
+N_SAMPLES = 10000
+SPLIT_RATIO = 0.8
+HISTORY_LEN = 10
+FORECAST_LEN = 1
+
+identity = lambda x: x
+
+
+# Load Mackey-Glass dataset
+mackey_data = jnp.squeeze(jnp.array(generate_mackey_glass(N_SAMPLES)))
+
+# Split into train-test
+split = int(SPLIT_RATIO * N_SAMPLES)
+mackey_train = mackey_data[:split]
+mackey_test = mackey_data[split:]
+
+# Split into input and output
+X_train, Y_train = chunkify(mackey_train, HISTORY_LEN, FORECAST_LEN)
+X_test, Y_test = chunkify(mackey_test, HISTORY_LEN, FORECAST_LEN)
+
+X_train_np, Y_train_np = np.array(X_train), np.array(Y_train)
+X_test_np, Y_test_np = np.array(X_test), np.array(Y_test)
+
+
+def fit_predict_esn_jax(x_train: Array, y_train: Array) -> Array:
+
+    esn_jax = ESN(
+        PRNGKEY,
+        hidden_nodes=500,
+        sparsity_in=1.0,
+        sparsity_node=1.0,
+        input_activation=identity,
+        node_activation=jax.nn.relu,
+        spectral_radius=1.0,
+        leakage=0.3156014208642396,
+        l2_cost=628.7829947402206
+    )
+    y_hat = esn_jax.fit(x_train, y_train).predict(x_train)
+    
+    return y_hat
+
+
+def fit_predict_esn_pyrcn(x_train: Array, y_train: Array) -> Array:
+
+    input_to_node = InputToNode(
+        hidden_layer_size=500,
+        k_in=None,
+        sparsity=1.0,
+        input_activation="identity",
+    )
+    node_to_node = NodeToNode(
+        hidden_layer_size=500,
+        sparsity=1.0,
+        reservoir_activation="relu",
+        spectral_radius=1.0,
+        leakage=0.3156014208642396,
+        bidirectional=False
+    )
+
+    R_i2n = input_to_node.fit_transform(x_train)
+    R_n2n = node_to_node.fit_transform(R_i2n)
+    ridge_train = Ridge(alpha=628.7829947402206).fit(R_n2n, y_train)
+    y_hat = ridge_train.predict(R_n2n)
+
+    return y_hat
+
+
+def measure_jax_times(repeat: int, number: int) -> Array:
+
+    setup_code = """
+from __main__ import fit_predict_esn_jax
+from __main__ import X_train, Y_train
+
+fit_predict_esn_jax(X_train, Y_train)
+"""
+
+    test_code = """
+fit_predict_esn_jax(X_train, Y_train)"""
+
+    jax_times = timeit.repeat(setup=setup_code, stmt=test_code, repeat=repeat, number=number)
+    return np.array(jax_times) / number
+
+
+def measure_pyrcn_times(repeat: int, number: int) -> Array:
+
+    setup_code = """
+from __main__ import fit_predict_esn_pyrcn
+from __main__ import X_train_np, Y_train_np
+
+fit_predict_esn_pyrcn(X_train_np.T, Y_train_np.T)"""
+
+    test_code = """
+fit_predict_esn_pyrcn(X_train_np.T, Y_train_np.T)"""
+
+    pyrcn_times = timeit.repeat(setup=setup_code, stmt=test_code, repeat=repeat, number=number)
+    return np.array(pyrcn_times) / number
+
+
+
+if __name__ == "__main__":
+
+    # Parse arguments
+    parser = ArgumentParser()
+    parser.add_argument('-r', '--repeat', default=25, type=int)
+    parser.add_argument('-n', '--number', default=100, type=int)
+    args = parser.parse_args()
+    
+    # Measure execution times
+    print("Measuring JAX execution times...")
+    jax_times = measure_jax_times(repeat=args.repeat, number=args.number)
+    print("Measuring PyRCN execution times...")
+    pyrcn_times = measure_pyrcn_times(repeat=args.repeat, number=args.number)
+
+    # Convert to dataframe
+    frameworks = ['JAX' for _ in range(args.repeat)] + ['PyRCN' for _ in range(args.repeat)]
+    df = pd.DataFrame({'Framework': frameworks, 'Time': np.concatenate((jax_times, pyrcn_times))})
+    df["All"] = ""
+
+    # Plot results
+    ax = sns.violinplot(x="All", y="Time", hue="Framework", data=df, split=True)
+    ax.set_xlabel("")
+    ax.set_ylabel("Execution Time (s)")
+    plt.title("Echo State Networks with different frameworks")
+    plt.savefig("exec_time.pdf")
+
+
+
